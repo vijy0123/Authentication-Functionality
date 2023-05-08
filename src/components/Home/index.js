1463cff15e9ffd8fc9cddcba2fb7483bc9f25aa2
vijy0123 <vijaydyagala0123@gmail.com>
2023-05-08 10:48:42 +0000

    Authentication Functionality

---
+++ src/components/Home/index.js
@@ -0,0 +1,18 @@
+// Write your JS code here
+
+import LogoutButton from '../LogoutButton'
+import Header from '../Header'
+
+import './index.css'
+
+const Home = () => (
+  <>
+    <Header />
+    <div className="home-container">
+      <h1>Home Route</h1>
+      <LogoutButton />
+    </div>
+  </>
+)
+
+export default Home
